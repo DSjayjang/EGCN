bcf1bd2066b05222215c0853473dcfae88d0b2a3
DSjayjang <a52071194@gmail.com>
2024-12-16 22:12:36 +0900

    a

--- exec_reg_freesolv_conf.py
+++ exec_reg_freesolv_conf.py
@@ -340,8 +340,8 @@ model_Outer_EGCN_elastic = Outer_EGCN_elastic.Net(mc.dim_atomic_feat, 1, mc.dim_
 
 
 # define loss function
-criterion = nn.L1Loss(reduction='sum') # MAE
-# criterion = nn.MSELoss(reduction='sum') # MSE
+# criterion = nn.L1Loss(reduction='sum') # MAE
+criterion = nn.MSELoss(reduction='sum') # MSE
 
 # train and evaluate competitors
 test_losses = dict()
